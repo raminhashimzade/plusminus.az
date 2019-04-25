6dfb980de1b573a604fb2fdb6dce13434c100b0b
Vugar Abdullayev <vugar.abdullayev@neuron.az>
2019-04-25 21:06:54 +0400

    landing page component added

--- src/app/home/landing-page/landing-page.module.ts
+++ src/app/home/landing-page/landing-page.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page.component';
 import { SharedTranslateModule } from 'src/app/shared/shared-translate.module';
+import { MainBannerComponent } from './main-banner/main-banner.component';
 
 @NgModule({
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, MainBannerComponent],
   imports: [
     CommonModule,
     SharedTranslateModule,
---
+++ src/app/home/landing-page/main-banner/main-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-banner',
+  templateUrl: './main-banner.component.html',
+  styleUrls: ['./main-banner.component.scss']
+})
+export class MainBannerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
