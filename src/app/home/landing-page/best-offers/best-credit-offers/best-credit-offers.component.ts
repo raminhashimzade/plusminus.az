0f2738dea90ef5000bbdaa401438152f8ef43382
vugar005 <abdullayev1.618@hotmail.com>
2019-05-12 12:25:51 -0700

    best credit offers swiper config improved

--- src/app/home/landing-page/best-offers/best-credit-offers/best-credit-offers.component.ts
+++ src/app/home/landing-page/best-offers/best-credit-offers/best-credit-offers.component.ts
@@ -8,12 +8,40 @@ declare var Swiper;
   styleUrls: ['./best-credit-offers.component.scss']
 })
 export class BestCreditOffersComponent implements OnInit, OnDestroy {
-    creidtOffers = MockCreditOffers;
-    swiper: any;
-    slidesPerView = 3;
-    public config: SwiperConfigInterface;
-    @HostListener('window:resize', ['$event']) resize() {this.onWindowResize(); }
-  constructor() { }
+  creidtOffers = MockCreditOffers;
+  swiper: any;
+  public config: SwiperConfigInterface = {
+    slidesPerView: 3,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    speed: 800,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-next'
+    },
+    breakpoints: {
+      1700: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      },
+      1199: {
+        slidesPerView: 3,
+        slidesPerGroup: 3
+      },
+      992: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      576: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      }
+    }
+  };
+  constructor() {}
 
   ngOnInit() {
     this.initSwiper();
@@ -21,50 +49,10 @@ export class BestCreditOffersComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.swiper && this.swiper.destroy();
   }
-  onWindowResize(): void {
-    this.initSwiper();
-  }
   initSwiper() {
-    const config = this.getSwiperConfig();
+    this.swiper && this.swiper.destroy();
     setTimeout(() => {
-      this.swiper && this.swiper.destroy();
-        this.swiper = new Swiper('.swiper-container', config);
+      this.swiper = new Swiper('.swiper-container', this.config);
     }, 0);
   }
-  getSwiperConfig() {
-    return {
-      slidesPerView: this.getSlidesPerView(),
-      slidesPerGroup: this.getSlidesPerView(),
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        speed: 1500,
-        spaceBetween: 10,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-next',
-        },
-        // autoplay: {
-        //   delay: 2500,
-        //   stopOnLastSlide: true
-        // },
-      };
-  }
-  getSlidesPerView(): number {
-    let value: number;
-    if (window.innerWidth > 1500) {
-      value = this.slidesPerView = 3;
-    } else if (window.innerWidth > 1199 && window.innerWidth < 1500) {
-     value = this.slidesPerView = 3;
-    } else if (window.innerWidth > 992 && window.innerWidth < 1199) {
-     value =  this.slidesPerView = 3;
-    } else if (window.innerWidth > 576 && window.innerWidth < 992) {
-      value =  this.slidesPerView = 2;
-     }  else if (window.innerWidth > 250 && window.innerWidth < 576) {
-      value =  this.slidesPerView = 1;
-     }
-    return value;
-  }
-
 }
