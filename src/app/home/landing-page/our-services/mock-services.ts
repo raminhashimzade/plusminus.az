20a985db610779200106eeffa93ae5aa9b5d3695
Vüqar Abdullayev <vugar.abdullayev@neuron.az>
2019-05-14 18:07:35 +0400

    minor fix in footer

--- src/app/home/landing-page/our-services/mock-services.ts
+++ src/app/home/landing-page/our-services/mock-services.ts
@@ -2,7 +2,8 @@ export const MockServices = [
   {
     name: '~deposits',
     iconPath: './assets/images/deposits.png',
-    url: ''
+    url: '',
+    disabled: true
   },
   {
     name: '~credits',
@@ -12,21 +13,24 @@ export const MockServices = [
   {
     name: '~credit-cards',
     iconPath: './assets/images/credit-cards.png',
-    url: ''
+    url: '',
+    disabled: true
   },
   {
     name: '~debit-cards',
     iconPath: './assets/images/debit-cards.png',
-    url: ''
+    url: '',
+    disabled: true
   },
   {
     name: '~auctions',
     iconPath: './assets/images/mobile-tariffs.png',
-    url: ''
+    url: '',
+    disabled: true
   },
   {
     name: '~exchange-rates',
     iconPath: './assets/images/exchange-rates.png',
-    url: ''
+    url: 'exchange-rates'
   }
 ];
