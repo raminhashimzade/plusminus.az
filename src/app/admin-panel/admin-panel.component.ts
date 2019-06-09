46379adbb2db3547b285342ebb0b83b41bff0041
vugar <vugar_abdullayev@outlook.com>
2019-06-09 07:31:13 -0700

    full screen feature added

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, HostListener } from '@angular/core';
+import { AdminPanelService } from './admin-panel.service';
+import { Key } from '../app.utils';
 
 @Component({
   selector: 'admin-panel',
@@ -6,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-panel.component.scss']
 })
 export class AdminPanelComponent implements OnInit {
-
-  constructor() { }
+  @HostBinding('class.--full-screen') isFullScreen: boolean;
+  @HostListener('document:keydown', ['$event'])
+  handleEsc(event: KeyboardEvent) {
+    if (event.keyCode === Key.Escape) {
+      event.preventDefault();
+      this.adminPanelService.fullScreen$.next(false);
+    }
+  }
+  constructor(private adminPanelService: AdminPanelService) {
+    this.adminPanelService.fullScreen$.subscribe(
+      res => (this.isFullScreen = res)
+    );
+  }
 
   ngOnInit() {
   }
--- src/app/app.utils.ts
+++ src/app/app.utils.ts
@@ -131,3 +131,20 @@ export function loadExternalScripts(url: string) {
      document.head.appendChild(scriptElement);
    });
  }
+
+
+ export enum Key {
+  Backspace = 8,
+  Tab = 9,
+  Enter = 13,
+  Shift = 16,
+  Escape = 27,
+  ArrowLeft = 37,
+  ArrowRight = 39,
+  ArrowUp = 38,
+  ArrowDown = 40,
+  // http://unixpapa.com/js/key.html
+  MacCommandLeft = 91,
+  MacCommandRight = 93,
+  MacCommandFirefox = 224
+}
