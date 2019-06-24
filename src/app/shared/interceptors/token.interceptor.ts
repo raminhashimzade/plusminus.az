633fe0fe5c9a12441de540893636a9badc87e82b
vugar <vugar_abdullayev@outlook.com>
2019-06-24 11:29:12 -0700

    token interceptor added

---
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+import { mergeMap, shareReplay } from 'rxjs/operators';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+    fetchToken$ = this.authService.fetchToken().pipe(shareReplay(1));
+  constructor(private authService: AuthService) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (request.url.includes('mybank/getSessionId')) { return next.handle(request); }
+
+      return this.fetchToken$
+      .pipe(
+          mergeMap((token) => {
+            const authReq =  request.clone({
+                body: {
+                    ...request.body,
+                    token: token
+                }
+            });
+            return next.handle(authReq);
+          } )
+          );
+  }
+}
