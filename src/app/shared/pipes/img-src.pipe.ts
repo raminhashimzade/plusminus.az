5d48ea583c09b9ad4c803a2a97e315ff0af3b588
Vüqar Abdullayev <vugar.abdullayev@neuron.az>
2019-06-24 13:41:39 +0400

    img src pipe changed

--- src/app/shared/pipes/img-src.pipe.ts
+++ src/app/shared/pipes/img-src.pipe.ts
@@ -1,9 +1,7 @@
-import { Pipe, PipeTransform, ChangeDetectorRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Pipe, PipeTransform } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { map, distinctUntilChanged, switchMap } from 'rxjs/operators';
-import { Observable, Subject } from 'rxjs';
+import { getBaseUrl } from 'src/app/app.utils';
 
 @Pipe({
   name: 'imgSrc',
@@ -11,39 +9,11 @@ import { Observable, Subject } from 'rxjs';
 })
 export class ImgSrcPipe implements PipeTransform {
   hostname: string;
-  private valueSubject = new Subject();
-  imageFetcher$ =  this.valueSubject.asObservable()
-  .pipe(
-    distinctUntilChanged(),
-    switchMap(value => {
-      return  this.http.post('mybank/getFileById', {
-        fileId: value,
-        token: this.authService.token
-      }).pipe(
-        map((res: any) => {
-          if (!(res && res.data && res.data.file && res.data.fileExt)) {return;}
-          const base64 =  res.data.file;
-          const fileExt = res.data.fileExt;
-          let base64Header: string = 'data:image/png;base64';
-          // switch(fileExt) {
-          //   case 'svg':
-          //     base64Header = 'data:image/svg+xml;base64';
-          //     break;
-          //    case 'png':
-          //      base64Header = 'data:image/png;base64';
-          // }
-          return res && res.data && res.data.file && this.sanitizer.bypassSecurityTrustUrl(`${base64Header}, ${base64}`);
-        })
-      );
-    })
-  )
-  constructor(cdRef: ChangeDetectorRef, private http: HttpClient, private authService: AuthService, private sanitizer: DomSanitizer) {
+  constructor(private authService: AuthService, private sanitizer: DomSanitizer) {
   }
 
-  transform(fileId: string): Observable<SafeResourceUrl> {
+  transform(fileId: string): SafeResourceUrl {
     if (!fileId) {return;}
-    this.valueSubject.next(fileId);
-    return this.imageFetcher$;
-
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`${getBaseUrl()}/mybank/getFileById/${fileId}/${this.authService.getToken()}`)
   }
 }
