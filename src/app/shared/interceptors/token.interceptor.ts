a56a9ec0509c065343ee10f6cfead246f5cf06f4
Vüqar Abdullayev <vugar.abdullayev@neuron.az>
2019-06-25 10:53:37 +0400

    token interceptor catch error added

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
-import { mergeMap, shareReplay } from 'rxjs/operators';
+import { mergeMap, shareReplay, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    fetchToken$ = this.authService.fetchToken().pipe(shareReplay(1));
-  constructor(private authService: AuthService) {}
+    fetchToken$ = this.authService.fetchToken().pipe(
+      shareReplay(1),
+      catchError(er => {
+        this.router.navigateByUrl('error-page');
+         return of(er);
+      })
+      );
+  constructor(private authService: AuthService, private router: Router) {}
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (request.url.includes('mybank/getSessionId')) { return next.handle(request); }
     if (request.url.includes('.json')) { return next.handle(request); }
