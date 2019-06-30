1bc18c39173b644900f49daa779298ae86b4f322
vugar <vugar_abdullayev@outlook.com>
2019-06-30 09:09:00 -0700

    new offers added to credit and debit cards

--- src/app/home/landing-page/best-offers/best-credit-card-offers/mock-credit-card.offer.ts
+++ src/app/home/landing-page/best-offers/best-credit-card-offers/mock-credit-card.offer.ts
@@ -2,7 +2,16 @@ export const MockCreditCardOffer = [
     {
         percent: '19',
          duration: '36 ay',
-        iconPath: '1'
+        iconPath: '1',
+        name: "65 günədək 0% faizlə",
+        url: "http://azerturkbank.az/loans/gcreditcards/info/"
+    },
+    {
+        percent: '25',
+         duration: '60 ay',
+        iconPath: '12',
+        name: "Bolkart 45 günədək 0% ilə",
+        url: "https://www.bolkart.az/"
     },
 
 
