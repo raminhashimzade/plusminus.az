36acbff01732e90ed827128f62437ac44cfe60ab
vugar <vugar_abdullayev@outlook.com>
2019-06-30 09:18:07 -0700

    build get baseUrl method changed

--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -1,3 +1,5 @@
 import { environment } from 'src/environments/environment';
 
-export const baseUrl = environment.baseUrl;
+export const baseUrl =
+(window.location.origin.includes('test.plusminus.az') || window.location.origin.includes('localhost:4200') )
+? 'https://testapi.plusminus.az' : 'https://api.plusminus.az';
