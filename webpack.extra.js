875a59466b2439aa48f6aa68d21447f0626121a9
Vüqar Abdullayev <vugar.abdullayev@neuron.az>
2019-07-01 10:50:06 +0400

    copy plugin added to webpack.extra.js

--- webpack.extra.js
+++ webpack.extra.js
@@ -1,6 +1,11 @@
 const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new CopyPlugin([
+      { from: './src/assets/googlee009e07a42854a06.html', to: './' },
+    ])
   ]
 }
