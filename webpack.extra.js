e161463f1bf3d8f8ec46ce1c03d1602db27594fc
Vüqar Abdullayev <vugar.abdullayev@neuron.az>
2019-07-01 15:51:08 +0400

    google verification file added"

--- webpack.extra.js
+++ webpack.extra.js
@@ -4,8 +4,8 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // new CopyPlugin([
-    //   { from: './src/assets/googlee009e07a42854a06.html', to: './' },
-    // ])
+    new CopyPlugin([
+      { from: './src/assets/googlec2cdd7644fbce54f.html', to: './' },
+    ])
   ]
 }
