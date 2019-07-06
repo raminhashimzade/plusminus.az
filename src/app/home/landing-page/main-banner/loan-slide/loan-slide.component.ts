1782725b3f511d27fd8863c856abc2107fdd4b64
vugar <vugar_abdullayev@outlook.com>
2019-07-06 03:21:10 -0700

    typedJs integrated to loan slide

--- src/app/home/landing-page/main-banner/loan-slide/loan-slide.component.ts
+++ src/app/home/landing-page/main-banner/loan-slide/loan-slide.component.ts
@@ -1,29 +1,52 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { SlideModel } from '../slide.model';
 import { MatDialog } from '@angular/material/dialog';
 import { LoanRequestDialogComponent } from 'src/app/home/loans/loan-request-dialog/loan-request-dialog.component';
 import { isMobileSize } from 'src/app/app.utils';
+import Typed from 'typed.js';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'loan-slide',
   templateUrl: './loan-slide.component.html',
   styleUrls: ['./loan-slide.component.scss']
 })
-export class LoanSlideComponent implements OnInit {
-
+export class LoanSlideComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('shortInfo', { static: false }) shortInfo: ElementRef;
   @Input() slide: SlideModel;
-  constructor(private dialog: MatDialog) {}
+  typed: any;
+  constructor(private dialog: MatDialog, private translateService: TranslateService) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
   onRequestLoansFromAllBanks() {
-    const ref  = this.dialog.open(LoanRequestDialogComponent, {
+    const ref = this.dialog.open(LoanRequestDialogComponent, {
       panelClass: 'loanRequestDialog',
       autoFocus: false,
       maxWidth: '99vw',
       disableClose: true,
-      position: isMobileSize() && {top: '10px'}
-    //  backdropClass: 'loanRequestDialogBackdrop'
+      position: isMobileSize() && { top: '10px' }
+      //  backdropClass: 'loanRequestDialogBackdrop'
     });
   }
-
+  ngAfterViewInit() {
+    this.startTypedJs();
+  }
+  startTypedJs() {
+    const options = {
+      strings: [
+        this.translateService.instant('~selectCreditsShortInfo'),
+        this.translateService.instant('~selectCreditsShortInfoSecond')],
+      typeSpeed: 40,
+      starDelay: 200,
+      backSpeed: 10,
+      loop:true,
+      loopCount: 2,
+      showCursor: false,
+      smartBackspace: true
+    }
+    this.typed = new Typed("#slideLoanShortInfo", options);
+  }
+  ngOnDestroy() {
+    this.typed.destroy();
+  }
 }
